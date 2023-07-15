81d0764fadb01d8493f84b28ed837271e9a968c0
GabrielNBS <123820384+GabrielNBS@users.noreply.github.com>
2023-07-15 17:48:55 -0300

    configurando o catch

--- main.js
+++ main.js
@@ -1,43 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Endpoint da API do GitHub para obter informações do usuário
+document.addEventListener('DOMContentLoaded', function () {
     const endpoint = 'https://api.github.com/users/GabrielNBS';
+    const imgAvatar = document.getElementById('avatar'); // Elemento HTML para exibir a imagem do avatar do usuário
+    const profileName = document.getElementById('name'); // Elemento HTML para exibir o nome do perfil do usuário
+    const profileUserName = document.getElementById('user-name'); // Elemento HTML para exibir o nome de usuário do perfil
+    const repositorios = document.getElementById('repository'); // Elemento HTML para exibir o número de repositórios públicos do usuário
+    const seguidores = document.getElementById('followers'); // Elemento HTML para exibir o número de seguidores do usuário
+    const seguindo = document.getElementById('following'); // Elemento HTML para exibir o número de pessoas que o usuário está seguindo
+    const link = document.getElementById('link'); // Elemento HTML para exibir o link para o perfil do usuário
 
-    // Selecionando elementos do HTML com base em seus IDs
-    const imgAvatar = document.getElementById('avatar'); // Imagem do avatar do usuário
-    const profileName = document.getElementById('name'); // Nome do perfil do usuário
-    const profileUserName = document.getElementById('user-name'); // Nome de usuário do perfil
-    const repositorios = document.getElementById('repository'); // Número de repositórios públicos do usuário
-    const seguidores = document.getElementById('followers'); // Número de seguidores do usuário
-    const seguindo = document.getElementById('following'); // Número de pessoas que o usuário está seguindo
-    const link = document.getElementById('link'); // Link para o perfil do usuário
+    let apiSuccess = true; // Variável para verificar se a API foi carregada com sucesso
 
-    // Realizando uma solicitação HTTP GET para o endpoint da API do GitHub
     fetch(endpoint)
-    .then(function (res) {
-        return res.json(); // Convertendo a resposta em formato JSON
-    })
-    .then(function(json) {
-        // Atualizando os elementos HTML com os dados obtidos da resposta JSON
-
-        // Definindo o URL da imagem do avatar do usuário
-        imgAvatar.src = json.avatar_url;
-
-        // Definindo o nome do perfil do usuário
-        profileName.innerText = json.name;
-
-        // Definindo o nome de usuário do perfil
-        profileUserName.innerText = json.login;
-
-        // Definindo o número de repositórios públicos do usuário
-        repositorios.innerText = json.public_repos; 
-
-        // Definindo o número de seguidores do usuário
-        seguidores.innerText = json.followers;
-
-        // Definindo o número de pessoas que o usuário está seguindo
-        seguindo.innerText = json.following;
-
-        // Definindo o link para o perfil do usuário
-        link.href = json.html_url;
-    });
+        .then(function (res) {
+            if (res.ok) { // Verifica se a resposta da API tem status 200 (OK)
+                return res.json(); // Converte a resposta em formato JSON
+            } else {
+                apiSuccess = false; // Define a variável como false caso ocorra um erro na resposta da API
+                throw new Error('Erro na resposta da API');
+            }
+        })
+        .then(function (json) {
+            if (apiSuccess) { // Verifica se a API foi carregada com sucesso antes de atualizar os elementos HTML
+                imgAvatar.src = json.avatar_url; // Define a imagem do avatar do usuário
+                profileName.innerText = json.name; // Define o nome do perfil do usuário
+                profileUserName.innerText = json.login; // Define o nome de usuário do perfil
+                repositorios.innerText = json.public_repos; // Define o número de repositórios públicos do usuário
+                seguidores.innerText = json.followers; // Define o número de seguidores do usuário
+                seguindo.innerText = json.following; // Define o número de pessoas que o usuário está seguindo
+                link.href = json.html_url; // Define o link para o perfil do usuário
+            }
+        })
+        .catch(function (erro) {
+            console.log('Ocorreu um erro na solicitação da API:', erro);
+            if (!apiSuccess) { // Verifica se ocorreu um erro na resposta da API antes de exibir a mensagem de erro
+                alert('Ocorreu um erro na solicitação da API');
+            }
+        });
 });
