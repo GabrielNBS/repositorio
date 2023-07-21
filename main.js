6b92b1568fb66fd2f7bfdf1503ca30004e5e5d3c
GabrielNBS <123820384+GabrielNBS@users.noreply.github.com>
2023-07-21 20:41:58 -0300

    finalizando exercicio

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-const alunos = new Map();
-alunos.set("nome" , "gabriel");
+const nomes = ["gabriel"];
 
-console.log(alunos)
+const alunos = nomes.map(itemAtual => itemAtual = {
+    nome: itemAtual,
+    nota: 6
+})
+
+alunos.push({
+    nome: "gabrielly",
+    nota: 10
+})
+
+alunos.push({
+    nome: "mayumi",
+    nota: 3
+})
+
+const aprovados = item => item.nota >= 6;
+
+const alunoAprovados = alunos.filter(aprovados);
+
+
+console.log(alunoAprovados)
