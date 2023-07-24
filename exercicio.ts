e6217b3209bcc015673f56e7faf68ed707d58f54
GabrielNBS <123820384+GabrielNBS@users.noreply.github.com>
2023-07-24 20:49:44 -0300

    finalizando exercicio

---
+++ exercicio.ts
@@ -0,0 +1,7 @@
+const multiplicacao = (num1: number, num2: number): number => num1 * num2;
+
+console.log(multiplicacao(2, 7));
+
+const saudacao = (nome: string): string => `Olá ${nome}!`
+
+console.log(saudacao("gabriel"));
